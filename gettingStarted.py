a511e93d91cd59fb96f6eac9f6c5f4912f7488f6
abrarrahman23 <54648684+abrarrahman23@users.noreply.github.com>
2025-09-14 04:53:50 -0400

    Updated gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -44,16 +44,3 @@ def welcome_assignment_answers(question):
 #"Is MD5 a secured hashing algorithm? - Yes/No":
 #"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
 #"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-
-
-
-#Questions:
-#"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-#"Are encoding and encryption the same? - Yes/No":
-#"Is it possible to decrypt a message without a key? - Yes/No":
-#"Is it possible to decode a message without a key? - Yes/No":
-#"Is a hashed message supposed to be un-hashed? - Yes/No":
-#"What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
-#"Is MD5 a secured hashing algorithm? - Yes/No":
-#"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-#"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
